6dbac391b9bb8103dcb58219476da81ef872c4cb
Joshua Peisach <itzswirlz2020@outlook.com>
2024-01-04 10:43:40 -0500

    reverse/flagtureiser (#7)
    * start reverse/flagtureiser
    
    * Copy the forge MDK for 1.19.4 (latest version used for modding at the time), create the flag obfuscation
    
    * fix deploy stuff
    
    ---------
    
    Co-authored-by: mudasir <96320211+mud-ali@users.noreply.github.com>

---
+++ flagtureiser/source/flagcreator.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		String e = "bcactf{fRaCtur31s3R_sT8gE_z3R0}";
+		byte[] f = e.getBytes();
+		for(byte b : f) {
+			System.out.print(b + ", ");
+		}
+	}
+}
