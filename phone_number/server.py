20a2baef1afa212839855b36409c9ce920cd5f97
Korn-Jacob <jackor27@bergen.org>
2024-02-27 15:21:50 -0500

    here's the actual server added, yw mudasir:

---
+++ phone_number/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+flag = open("flag.txt","r").read()
+
+@app.route("/")
+def mainpage():
+    return open("index.html","r").read() + \
+        "<script>" + open("script.js","r").read() + "</script></html>"
+
+@app.route("/flag", methods=["POST"])
+def get_flag():
+    print(request.data)
+    if request.data == b"1234567890":
+        return flag
+    else:
+        return ""
