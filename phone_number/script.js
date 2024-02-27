4d7aa6f1e76c917c77f3c962c4b37ba09ebdc0f6
Korn-Jacob <jackor27@bergen.org>
2024-02-27 12:43:44 -0500

    fixed code formatting and added a webserver with flask

--- phone_number/script.js
+++ phone_number/script.js
@@ -1,65 +1,54 @@
-var die1 = 0;
-var die2 = 0;
-var subtract = false;
-let result = document.getElementById("result");
-let input = document.getElementById("input");
-let dicetext = document.getElementById("dicetext");
-let img1 = document.getElementById("img1");
-let img2 = document.getElementById("img2");
-let resultbuttons = document.getElementsByClassName("resultbutton")
+	var DiE1	 = 0	 ;
+	var DiE2		= 0 ;
+ var SubTraCT		= false  ;
+	let RESulT = document	 .getElementById  ("result"		) ;
+ let InPuT	 = document	 .getElementById	 ("input"	 )  ;
+		let dicETEXT	= document .getElementById	("dicetext"  )	;
+  let iMg1	 = document	.getElementById		("img1"		)	;
+	let Img2 = document  .getElementById	 ("img2"		)		;
+ let REsuLtbUTtOnS		= document  .getElementsByClassName ("resultbutton"  )
 
-input.value = ""; 
+	InPuT	.value	= "" ;
 
 
-input.onclick = () => { // can't turn off readonly now
-    input.readOnly = true;
-}
-input.onchange = () => { // now you really can't edit it
-    input.readOnly = true;
-    input.value = "";
-}
-document.body.onkeydown = () => { // now you REALLY can't edit it
-    input.readOnly = true;
-}
+	InPuT  .onclick	= (	 )	=> { // can't turn off readonly now
+ InPuT	 .readOnly	 = true	;	}
+ InPuT	.onchange	 =  (  ) => { // now you really can't edit it
+	InPuT	.readOnly	 = true	;
+  InPuT	 .value		= ""	 ;	 }
+  document  .body .onkeydown =	(  )	 =>	 { // now you REALLY can't edit it
+	 InPuT	.readOnly	= true		; }
 
-function rollDice() {
-    die1 = Math.floor(Math.random()*6)+1;
-    die2 = Math.floor(Math.random()*6)+1;
-    if (input.value === "123456789") { 
-        die1 = 1;
-        die2 = 1;
-    } // lol
-    img1.src = "./images/" + die1 + ".jpeg";
-    img2.src = "./images/" + die2 + ".jpeg";    
-    subtract = false;
-    dicetext.innerHTML = "You rolled a " + die1 + " and a " + die2 + ", for a result of " + (subtract ? Math.abs(die1-die2) : (die1 + die2)) + "."
-    for (let r of resultbuttons) r.style.display = "inline";
-    if (1 == die1 == die2) {
-        dicetext.innerHTML = "Snake eyes! Your input has been reset.";
-        input.value = "";
-        for (let r of resultbuttons) r.style.display = "none";
-    }
-    result.style.display = "inline";
-    
-}
+		function rollDice	(	 )	{
+	 DiE1		= Math	 .floor	 (Math  .random  (		)	*6	 )  +1		;
+	 DiE2	= Math  .floor		(Math	 .random  ( )  *6		)		+1  ;
+	 if		(InPuT	.value === "123456789"  )	 {
+	 DiE1	 = 1	 ;
+	 DiE2 = 1  ;		}
+		iMg1		.src	= "./static/images/"	 + DiE1 + ".jpeg"  ;
+	 Img2	.src = "./static/images/"  + DiE2  + ".jpeg"	;
+ SubTraCT = false	 ;
+		dicETEXT	 .innerHTML	 = "You rolled a " + DiE1		+ " and a " + DiE2		+ ", for a result of "	 +	(SubTraCT	 ? Math  .abs (DiE1	 -DiE2	)  : (DiE1 + DiE2 )	) + "."
+		for (let R of REsuLtbUTtOnS		) R	.style  .display	= "inline"	;
+	 if	(1 == DiE1 == DiE2	 )		{
+	dicETEXT	 .innerHTML = "Snake eyes! Your input has been reset."  ;
+	InPuT	 .value	 = "" ;
+ for  (let R of REsuLtbUTtOnS  ) R	 .style .display	= "none"	;	 }
+	RESulT		.style  .display = "inline" ;	 }
 
-function addNumber() {
-    input.value += (subtract ? Math.abs(die1-die2) : die1+die2);
-    result.style.display = "none";
-}
-function submit() {
-    if (!input.value) {
-        alert("Please enter a phone number.");
-        return;
-    }
-    var c = confirm("Is " + input.value + " the correct phone number?");
-    if (!c) return;
-    if (input.value === "1234567890") fetch("flag.txt").then((res) => res.text()).then((text) => {
-        document.body.innerHTML = text;
-    });
-}
+  function addNumber	(	)  {
+	InPuT	 .value  +=  (SubTraCT ? Math	 .abs (DiE1	 -DiE2	 )  : DiE1	+DiE2		)  ;
+		RESulT		.style .display		= "none"	 ;		}
+ async function submit  ( ) {
+		if (!InPuT	 .value		)	{
+	alert		("Please enter a phone number."	 )		;
+ return  ;		}
+		var c		= confirm	("Is "		+ InPuT  .value		+ " the correct phone number?"	)	;
+ if		(!c ) return ;
+	 await fetch	 ('/flag'		,	{
+	 method : "POST" ,
+	 body : InPuT		.value	 }	 ) .then ((res	) => res		.text		(	) )		.then	((text	)	=> text		.length !== 0		? document		.body	 .innerHTML		= text	 : alert  ("Sorry, incorrect."	 ) )		; }
 
-function subtractSet() {
-    subtract = true;
-    dicetext.innerHTML = "You rolled a " + die1 + " and a " + die2 + ", for a result of " + (subtract ? Math.abs(die1-die2) : (die1 + die2)) + ".";
-}
+		function setSubtract	 (	 )		{
+	SubTraCT	= true	;
+	dicETEXT .innerHTML		= "You rolled a "		+ DiE1  + " and a "	+ DiE2 + ", for a result of " +	 (SubTraCT	? Math  .abs  (DiE1	-DiE2		)	: (DiE1 + DiE2	 )  )	+ "."		;	 }
