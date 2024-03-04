443ff3dc40a49d972c6dfc11342e0d8013d57a53
Korn-Jacob <jackor27@bergen.org>
2024-03-04 11:46:45 -0500

    fixed challenge (made it solvable)

--- js-learning/server.js
+++ js-learning/server.js
@@ -4,7 +4,6 @@ const port = 3000;
 const parser = require("body-parser");
 
 
-const flag = "bcactf{1ava5cRIPT_mAk35_S3Nse_48129846}";
 
 
 app.use("/", express.static("static"));
@@ -16,7 +15,10 @@ app.get("/", (req, res) => {
 
 
 app.post("/check", (req, res) => {
+    let flag = "bcactf{1ava5cRIPT_mAk35_S3Nse_48129846}"; // TODO: get from flag.txt instead of assigning it in the server file that the user can see
+
     let d = req.body;
+    let out = "";
     for (let i of ["[", "]", "(", ")", "+", "!"]) {
         d = d.replaceAll(i, "");
     }
@@ -25,10 +27,27 @@ app.post("/check", (req, res) => {
         return;
     }
 
-    if (eval(req.body) === "fun") {
-        res.send('Congratulations! You win the level!');
-    } else {
-        res.send("Unfortunately, you are incorrect. Try again.");
+    let c;
+    try {
+        c = eval(req.body).toString();
+    } catch (e) {
+        res.send("An error occurred with your code.");
     }
+
+    // disallow code execution
+    try {
+        if (typeof (eval(c)) === "function") {
+            res.send("Attempting to abuse javascript code against jslearning.site is not allowed under our terms and conditions.");
+        }
+    } catch (e) {}
+
+
+    out += "Checking the string " + c + "...|";
+    if (c === "fun") {
+        out+='Congratulations! You win the level!';
+    } else {
+        out+="Unfortunately, you are incorrect. Try again.";
+    }
+    res.send(out);
 });
 app.listen(port, () => {});
--- js-learning/static/script.js
+++ js-learning/static/script.js
@@ -3,5 +3,9 @@ async function check() {
     await fetch('/check', {
         method:'POST',
         body: code.innerText
-    }).then((res)=>res.text()).then((text)=>alert(text))
+    }).then((res)=>res.text()).then((text)=>{ console.log(text.split("|"));
+        for (let i of text.split("|")) {
+            alert(i);
+        }
+    })
 }
