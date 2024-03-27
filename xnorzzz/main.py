fc3be2face5fdd6c30b93baf2f5c80c98c9d5494
camelPhi <mpylypov@gmail.com>
2024-03-27 15:30:58 -0400

    fixed comments

--- xnorzzz/main.py
+++ xnorzzz/main.py
@@ -1,5 +1,6 @@
 given = input("Enter your least favorite number: ").strip()
 
+# input list of hex numbers
 numbers = ["BDC9B3CBA3",
            "FDCC8EDEEB",
            "F9CD1B89C1",
@@ -16,28 +17,40 @@ numbers = ["BDC9B3CBA3",
            "89E9970ABB",
            "DCF91AC8A1"]
 
+# list of binary numbers converted from hex
 binNumber = []
+# iterating through the numbers
 for b in range(len(numbers)):
+    # convert to decimal from hex
     hexi = int(numbers[b], 16)
+    # convert to binary from decimal
     binary = bin(hexi)
+    # remove the '0x' part from above
     yex = binary[2:]
+    # adding converted binary number to list
     binNumber.append(yex)
 
 string = ""
+# iterate through each bit of binary numbers
 for c in range(len(binNumber[0])):
-    same = True
-    prev = binNumber[0][c]
-    for a in range(1,len(binNumber)):
-        if binNumber[a][c] != prev:
-            same = False
-            break
-    if not same:
-        string += "0"
-    else:
+    summ = 0
+    # iterate through all the binary numbers
+    for d in range(len(binNumber)):
+        # add the same bit of each binary number
+        summ += binNumber[d][c]
+    # checking if the numbers are all 0 or all 1
+    if summ == 0 or summ == 15:
+        # adding one according to XNOR algorithm
         string += "1"
+    else:
+        # adding zero according to XNOR algorithm
+        string += "0"
 
+# converting the string to decimal and then to hex
 finHex = hex(int(string,2))
+# remove the '0x' part from above
 finHex = finHex[2:]
+# print answer
 if given == finHex:
     print("Good job! Have a cookie: bcactf{th3fl@gUsee33ek_g3tg00d}")
 else:
