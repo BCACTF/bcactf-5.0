4fd440c1bb2c354b93de6a3eac7afc50b1553fb6
ZeviCohen <123010327+ZeviCohen@users.noreply.github.com>
2024-04-10 16:31:15 -0400

    told user how to exit

--- mathjail/pycalculator.py
+++ mathjail/pycalculator.py
@@ -3,5 +3,5 @@ equation = input("Enter your equation below and I will give you the answer:\n")
 while equation!="e":
     answer = eval(equation, {"__builtins__":{}},{})
     print(f"Here is your answer: {answer}")
-    equation = input("Enter your next equation below:\n")
+    equation = input("Enter your next equation below (type 'e' to exit):\n")
 print("Goodbye!")
