6a29de8be1b5834ebf294ce662aba2d1ed69067a
pinuna27 <pinuna27@bergen.org>
2024-04-10 16:04:27 -0400

    added first riddle

--- sea-scavenger/server.py
+++ sea-scavenger/server.py
@@ -8,5 +8,37 @@ app.static_folder = 'images'
 def home():
     return render_template('sea.html')
 
+@app.route('/shark')
+def shark():
+    return render_template('shark.html')
+
+@app.route('/squid')
+def squid():
+    return render_template('squid.html')
+
+@app.route('/clam')
+def clam():
+    return render_template('clam.html')
+
+@app.route('/shipwreck')
+def shipwreck():
+    return render_template('shipwreck.html')
+
+@app.route('/whale')
+def whale():
+    return render_template('whale.html')
+
+@app.route('/dolphin')
+def dolphin():
+    return render_template('dolphin.html')
+
+@app.route('/jellyfish')
+def jellyfish():
+    return render_template('jellyfish.html')
+
+@app.route('/treasure')
+def treasure():
+    return render_template('treasure.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
