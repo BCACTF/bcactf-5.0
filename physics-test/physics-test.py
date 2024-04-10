77d3b8c64c7b9f92da13f50d89ca16d7ef2bf380
glacialcascade <11969863+glacialcascade@users.noreply.github.com>
2024-04-10 11:25:17 -0400

    cooking

--- physics-test/physics-test.py
+++ physics-test/physics-test.py
@@ -3,24 +3,33 @@ from random import randint, choice
 flag = open("flag.txt", "r").read()
 
 def clear():
-    print("\n\n-----------------------------------\n\n")
+    print("\n\n---------------------------------------------------------------------\n\n")
 print("Welcome to the Midterm Review!")
 print("This review will test your knowledge of physics formulas we have learned this unit.")
+print("Be sure to write all of your answers in terms of x and y!")
 clear()
 
-questions = [("Question 1: A box starts at rest on a frictionless table. It has a constant acceleration of a. How far does it travel in b seconds?", "a*b*b/2")]
+questions = [("Question {}: A box starts at rest on a frictionless table. It has a constant acceleration of x. How far does it travel in y seconds?", "x*y*y/2"),
+             ("Question {}: A spring has a spring constant of x. If it is compressed by a distance of y, what is the magnitude of the restoring force? (Your answer should be positive.)", "x*y"),
+             ("Question {}: A red ball (with mass 1kg and velocity x) and a blue ball (with mass 2kg and velocity y collide inelastically). What is the final velocity of the two balls?", "(x+2*y)/3"),]
 
+i = 0
 while True:
+    i += 1
     q = choice(questions)
-    print(q[0])
+    print(q[0].format(i))
     answer = input("Answer: ")
+    for char in ".=_{}'\"" :
+        if char in answer:
+            print(f"Dangerous character {char}!")
+            exit()
     print("Running tests...")
     
     for _ in range(10):
-        a = randint(0, 100)
-        b = randint(0, 100)
-        expected = eval(q[1],  {'__builtins__': None}, {'a': a, 'b': b, 'flag': flag}) 
-        res = eval(answer, {'__builtins__': None}, {'a': a, 'b': b, 'flag': flag})
+        x = randint(0, 100)
+        y = randint(0, 100)
+        expected = eval(q[1], {'__builtins__': None, 'ord': ord, 'len': len}, {'x': x, 'y': y, 'flag': flag}) 
+        res = eval(answer, {'__builtins__': None, 'ord': ord, 'len': len}, {'x': x, 'y': y, 'flag': flag})
         if expected != res:
             print(f"TEST FAILED!")
             break
