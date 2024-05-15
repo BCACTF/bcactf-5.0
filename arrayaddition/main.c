4402b1d9eb20043aeb2fc299ac39f5f37fb14bcd
Joshua Peisach <itzswirlz2020@outlook.com>
2024-05-15 15:51:57 -0400

    rev/arrayaddition

---
+++ arrayaddition/main.c
@@ -0,0 +1,17 @@
+// flag is this
+// bcactf{w0W_cHa4aCt3R_aDD}
+#include <stdio.h>
+
+char arr[30] = {'b', 'b', '_', '`', 'p', 'a', 'u', 'p', '(', 'N', 'U', 'X', '<', 'T', '&', 'R', '3', 'c', '!', '?', 'K', 'L', '.', '-', 'e'};
+int main() {
+    // this will show how the text was generated which you can find w/ strings
+    // for(int i = 0; i < 25; i++) {
+    //     printf("%c", arr[i] + i);
+    // }
+
+    // solution:
+    // the same as above but subtract i instead of add
+
+    printf("i hope you can add numbers");
+    return 0;
+}
