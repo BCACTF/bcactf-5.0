40bf02086d1ee89d671a03c9c1e70978c6491f44
camelPhi <mpylypov@gmail.com>
2024-05-15 16:07:35 -0400

    fix

--- xnorzzz/main.py
+++ xnorzzz/main.py
@@ -1,6 +1,10 @@
+# we convert all the given hexadecimal numbers and convert them to binary
+# if all the bits at a certain position are the same, a 1 is added and a 0 is added otherwise
+# using this algorithm on all the bit positions, we get a new binary number of the same length
+# convert it back to hexadecimal and compare it to the given input for the flag
+
 given = input("Enter your least favorite number: ").strip()
 
-# input list of hex numbers
 numbers = ["BDC9B3CBA3",
            "FDCC8EDEEB",
            "F9CD1B89C1",
@@ -17,40 +21,25 @@ numbers = ["BDC9B3CBA3",
            "89E9970ABB",
            "DCF91AC8A1"]
 
-# list of binary numbers converted from hex
 binNumber = []
-# iterating through the numbers
 for b in range(len(numbers)):
-    # convert to decimal from hex
     hexi = int(numbers[b], 16)
-    # convert to binary from decimal
     binary = bin(hexi)
-    # remove the '0x' part from above
     yex = binary[2:]
-    # adding converted binary number to list
     binNumber.append(yex)
 
 string = ""
-# iterate through each bit of binary numbers
 for c in range(len(binNumber[0])):
     summ = 0
-    # iterate through all the binary numbers
     for d in range(len(binNumber)):
-        # add the same bit of each binary number
         summ += int(binNumber[d][c])
-    # checking if the numbers are all 0 or all 1
     if summ == 0 or summ == 15:
-        # adding one according to XNOR algorithm
         string += "1"
     else:
-        # adding zero according to XNOR algorithm
         string += "0"
 
-# converting the string to decimal and then to hex
 finHex = hex(int(string,2))
-# remove the '0x' part from above
 finHex = finHex[2:]
-# print answer
 if given == finHex:
     print("Good job! Have a cookie: bcactf{th3fl@gUsee33ek_g3tg00d}")
 else:
