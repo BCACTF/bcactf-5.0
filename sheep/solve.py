657d24dca70381b143c12b3bdbcb64fdc94e308b
glacialcascade <11969863+glacialcascade@users.noreply.github.com>
2024-05-23 18:50:56 -0400

    more WIP

---
+++ sheep/solve.py
@@ -0,0 +1,8 @@
+# I made this challenge by converting svg
+# to GeoJSON on https://labs.mapbox.com/svg-to-geojson/
+# converting it into a shapefile in QGIS
+# and zeroing out the header.
+# Note that generally, just a .shp file is not
+# enough to open a shapefile - you also need
+# various types of auxiliary indices - but
+# the .shp file contains all the polygon data.
