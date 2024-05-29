782f6827cc21160bb6f53e99b5e5f51bb1a53a35
pinuna27 <pinuna27@bergen.org>
2024-05-29 16:09:00 -0400

    hints!!!

--- sea-scavenger/resources/shipwreck.js
+++ sea-scavenger/resources/shipwreck.js
@@ -1,1 +1,2 @@
 console.log("Hint: Check the response headers");
+console.log("Also make sure to check \"/shipwreck\"");
--- sea-scavenger/resources/treasure.js
+++ sea-scavenger/resources/treasure.js
@@ -1,2 +1,3 @@
 console.log("Hint: what's robots.txt?");
 console.log("Another hint: I don't think the robots found the root! Check /treasure");
+console.log("Also - make sure to edit the URL");
