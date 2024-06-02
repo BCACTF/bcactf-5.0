953777565bbb799aa3a98117314aba78c1bd86be
pinuna27 <146117014+pinuna27@users.noreply.github.com>
2024-06-02 17:08:42 -0400

    Update sea-scavenger/resources/clam.js
    Co-authored-by: mudasir  <96320211+mud-ali@users.noreply.github.com>

--- sea-scavenger/resources/clam.js
+++ sea-scavenger/resources/clam.js
@@ -5,4 +5,4 @@ window.onbeforeunload = function() {
     document.cookie = "flag part 3:=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 };  
 
-console.log("Hint: check Application")
+console.log("Hint: how do websites remember you? Where do websites store things?")
