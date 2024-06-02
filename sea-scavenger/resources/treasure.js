a4b56d7b48d5508a9bbdc0849f4fc8b8922264ec
pinuna27 <146117014+pinuna27@users.noreply.github.com>
2024-06-02 17:10:06 -0400

    Update sea-scavenger/resources/treasure.js
    Co-authored-by: mudasir  <96320211+mud-ali@users.noreply.github.com>

--- sea-scavenger/resources/treasure.js
+++ sea-scavenger/resources/treasure.js
@@ -1,3 +1,3 @@
 console.log("Hint: what's robots.txt?");
-console.log("Another hint: I don't think the robots found the root! Check /treasure");
-console.log("Also - make sure to edit the URL");
+console.log("Another hint: I don't think the robots found the root! Check under /treasure");
+console.log("Also- this one isn't just about clicking around...");
