870547a7777ef9830a4558a0ebe3a8fca7764c0f
glacialcascade <11969863+glacialcascade@users.noreply.github.com>
2024-06-03 21:08:33 -0400

    rev/ghost (#49)
    * rev/ghost
    
    * Update Dockerfile
    
    * fix compilation
    
    * Hint
    
    ---------
    
    Co-authored-by: mudasir <96320211+mud-ali@users.noreply.github.com>

---
+++ ghost/ghost.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+char B = 11; 
+void m() { printf("ten"); } 
+int Y() { int k = 0; for(int i = 0; i < 9; i++) { k++; } return k; } 
+void four() { putchar(49); putchar(54); } 
+short f = 6; 
+void n() {printf("fift"); printf("een");}
+void t(int *a) { *a = 5;} 
+int M() { return (int) B + (int) f; } 
+void c() { int temp = 2; } 
+int c__ = 4;  
+char l() {return B + 2;} 
+int zero[] = {12}; 
+unsigned int leftbrace = 7; 
+char S = 'T' - 'A'; 
+long e = 18; 
+__uint64_t underscore = 14; 
+void Five() { printf("%lu", sizeof(double)); } 
+size_t rightBrace = 20;
+void b() { printf("1"); } 
+short a() { return 3; }
+
+int main() {
+    printf("...\n");
+    printf("no, you're not the one...");
+    return 0;
+}
