b98107de2f88f2afbd68e796e6385e4fda20e13f
Jack Crowley <98288502+Jack-Crowley@users.noreply.github.com>
2024-06-06 10:21:46 -0400

    Binex/canary keeper 0 (#65)
    * chall
    
    * spelling fix
    
    * deployment
    
    * flush buffers && change filename
    
    * deployment ha sido verificado
    
    ---------
    
    Co-authored-by: mudasir <96320211+mud-ali@users.noreply.github.com>
    Co-authored-by: glacialcascade <11969863+glacialcascade@users.noreply.github.com>

---
+++ canary-keeper-0/provided.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <stdio.h>
+
+#define CANARY_VALUE "canary\0"
+#define FLAG_VALUE "FLAG\0"
+
+int check_canary(const char* canary) {
+    return strcmp(canary, CANARY_VALUE) == 0;
+}
+
+int check_flag(const char* flag) {
+    return strcmp(flag, FLAG_VALUE) == 0;
+}
+
+int main() {
+    char flag[64] = FLAG_VALUE;
+    char canary[7] = CANARY_VALUE;
+    char buffer[64];
+
+    printf("Enter a string: ");
+    gets(buffer);
+
+    if (!check_canary(canary)) {
+        printf("Buffer overflow detected!\n");
+        return 1;
+    }
+
+    if (check_flag(flag)) {
+        printf("No changes in flag detected!\n");
+        return 1;
+    }
+
+    printf("Flag: %s\n", FLAG_VALUE);
+
+    return 0;
+}
