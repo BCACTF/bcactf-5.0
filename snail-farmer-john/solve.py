8656485b21d701029373b77e60e7d4873c975d7c
glacialcascade <11969863+glacialcascade@users.noreply.github.com>
2024-06-09 16:32:03 -0400

    clarify solution

--- snail-farmer-john/solve.py
+++ snail-farmer-john/solve.py
@@ -5,6 +5,7 @@
 
 fin = open("input.txt", "r")
 fout = open("fast.out", "w")
+
 # Read the input
 (n, k) = [int(x) for x in fin.readline().split(" ")]
 arr = [int(x) for x in fin.readline().split(" ")]
@@ -12,8 +13,8 @@ assert len(arr) == k
 
 sum = sum(arr)
 
-start = sum + n - k + 1
 out = 0
-for i in range(start, sum, -3):
-    out += (i * (i - 1) * (i - 2) * (i - 3)) // 8
+for i in range(n, k-1, -3):
+    num = sum + i - k # add i-k for the extra 1s at the end
+    out += ((num+1) * (num) * (num - 1) * (num - 2)) // 8
 fout.write("bcactf{" + str(out) + "}")
